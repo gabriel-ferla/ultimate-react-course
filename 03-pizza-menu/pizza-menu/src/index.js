49222e0835e715b79564c07cb8dd63ca4ef05c95
Gabriel Ferla <123134347+gabriel-ferla@users.noreply.github.com>
2024-04-20 21:25:11 -0300

    Update index.js

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -29,7 +29,7 @@ const pizzaData = [
       name: "Pizza Funghi",
       ingredients: "Tomato, mozarella, mushrooms, and onion",
       price: 12,
-      photoName: "pizzas/fun,,,,ghi.jpg",
+      photoName: "pizzas/funghi.jpg",
       soldOut: false,
     },
     {
@@ -71,11 +71,22 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese' photoName='pizzas/spinaci.jpg' price={12} />
+      <Pizza name='Pizza Funghi' ingredients='Tomato, mozarella, mushrooms, and onion' photoName='pizzas/funghi.jpg' price={12} />
     </main>
+  )
+}
+
+function Pizza(props) {
+  return (
+      <div className="pizza">
+          <img src={props.photoName} alt={props.name} />
+          <div>
+            <h3>{props.name}</h3>
+            <p>{props.ingredients}</p>
+            <span>{props.price}</span>
+          </div>
+      </div>
   )
 }
 
@@ -99,15 +110,7 @@ function Footer() {
   //return React.createElement("footer", null, "We're currently open!")
 }
 
-function Pizza() {
-    return (
-        <div>
-            <img src="pizzas/spinaci.jpg" alt="spinaci pizza" />
-            <h3>Pizza Spinaci</h3>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-    )
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
