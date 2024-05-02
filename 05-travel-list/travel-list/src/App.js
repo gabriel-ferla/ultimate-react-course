219ec0e54c5d3cb6f77fb7867dff57e3a1dc28b1
Gabriel Ferla <123134347+gabriel-ferla@users.noreply.github.com>
2024-05-01 22:34:12 -0300

    travel-list

---
+++ 05-travel-list/travel-list/src/App.js
@@ -0,0 +1,62 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo () {
+  return (
+    <h1>Far Away</h1>
+  )
+}
+
+function Form () {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  )
+}
+
+function PackingList () {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => <Item item={item}/>)}
+      </ul>
+    </div>
+  )
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      {item.quantity} {item.description} 
+      </span>
+      <button>&times;</button>
+    </li>
+  )
+}
+
+function Stats () {
+  return (
+    <footer className="staats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
+
+
+export default App;
