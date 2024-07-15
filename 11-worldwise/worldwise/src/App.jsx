4d64ec838326c0f237bf1f0bfdc9b7b70012c2e6
Gabriel Ferla <123134347+gabriel-ferla@users.noreply.github.com>
2024-07-15 13:13:32 -0300

    navigate bt, map, form

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 import Homepage from './pages/Homepage'
@@ -10,6 +10,7 @@ import PageNotFound from './pages/PageNotFound'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import City from './components/City'
+import Form from './components/Form'
 
 const BASE_URL = "http://localhost:8000"
 
@@ -41,11 +42,11 @@ function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route index element={<Navigate replace to='cities' />} />
           <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path='cities/:id' element={<City />} />
           <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path='form' element={<p>Form</p>} />
+          <Route path='form' element={<Form />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
--- 11-worldwise/worldwise/src/components/City.jsx
+++ 11-worldwise/worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,8 +10,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const x = useParams()
-    console.log(x)
+  const {id} = useParams()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
     
   // TEMP DATA
   const currentCity = {
@@ -22,7 +24,9 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-    return <h1>City {id}</h1>
+  return <>
+    <h1>City {id}</h1><p>Position: {lat}, {lng}</p>
+    </>
 
   // return (
   //   <div className={styles.city}>
